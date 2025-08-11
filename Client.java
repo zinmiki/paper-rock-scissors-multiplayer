6ff8043e1cf1f39d483bde8a1b96cea4d1f87bd2
Mikołaj Zinkiewicz <zinmiki10@gmail.com>
2025-08-11 19:01:38 +0200

    Multiplayer2

--- Client.java
+++ Client.java
@@ -5,18 +5,36 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+    public static void printStats(String input) {
+        // Rozdzielamy ciąg po "/"
+        String[] parts = input.split("/");
+
+        // Sprawdzamy, czy mamy dokładnie 3 elementy
+        if (parts.length != 3) {
+            System.out.println("[ERROR] Unkown format data!");
+            return;
+        }
+
+        // Wyświetlamy wynik
+        System.out.println("== WINS STATS ==");
+        System.out.println("Paper: " + parts[0]);
+        System.out.println("Rocks: " + parts[1]);
+        System.out.println("Scissors: " + parts[2]);
+        System.out.println("=============");
+    }
+
     public static String selectItem() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("==Please select an item from the list:==");
-        System.out.println("1. papier");
-        System.out.println("2. kamien");
-        System.out.println("3. nozyce");
-        System.out.print("Select an item: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("==Please select an item from the list:==");
+            System.out.println("1. paper");
+            System.out.println("2. rock");
+            System.out.println("3. scissors");
+            System.out.print("Select an item: ");
 
-        String item = scanner.nextLine();
-
-        System.out.println("You selected: " + item);
-        return item;
+            String item = scanner.nextLine();
+            System.out.println("Waiting for player choises..");
+            return item;
+        }
     }
     public static void main(String[] args) {
         if (args.length < 2) {
@@ -28,6 +46,7 @@ public class Client {
 
         try (Socket socket = new Socket(host, port)) {
             System.out.println("Connected to server at " + host + ":" + port);
+            System.out.println("\nWaiting for all players to connect...");
             
             BufferedReader in = new BufferedReader(new java.io.InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -39,13 +58,41 @@ public class Client {
                     System.out.println("Server disconnected.");
                     break;
                 }
-
-                switch (msg) {
-                    case "ITEM" -> {
-                        String item = selectItem();
-                        out.println(item);
+                if (Character.isDigit(msg.charAt(0))) {
+                    printStats(msg);
+                } else {
+                    switch (msg) {
+                        case "ITEM" -> {
+                            String item = selectItem();
+                            out.println(item);
+                        }
+                        case "WIN_p" -> {
+                            System.out.println("@ Paper wins!");
+                        }
+                        case "WIN_k" -> {
+                            System.out.println("@ Rocks wins!");
+                        }
+                        case "WIN_n" -> {
+                            System.out.println("@ Scissors wins!");
+                        }
+                        case "DRAW" -> {
+                            System.out.println("@ Draw! No one won");
+                        }
+                        case "LIMIT" -> {
+                            System.out.println("ROUND LIMIT! Thanks for playing. See you soon");
+                        }
+                        case "KICK" -> {
+                            try {
+                                System.out.println("\nYou are kicked from the server");
+                                socket.close();
+                                break;
+                            } catch (IOException e) {
+                                System.out.println("Failed to close socket "+ e.getMessage());
+                            }
+                        }
+                        case "PING" -> {}
+                        default -> System.out.println("Server direct message: " + msg);
                     }
-                    default -> System.out.println("Server direct message: " + msg);
                 }
             }
         
--- Server/Server.java
+++ Server/Server.java
@@ -35,15 +35,26 @@ public class Server {
     public static List<PlayerHandler> players = new ArrayList<>();
     public static Map<PlayerHandler, String> playerItems = new HashMap<>();
 
+    public static int winsP = 0;
+    public static int winsK = 0;
+    public static int winsN = 0;
+
     // Main method to start the server
     public static void main(String[] args) {
-        String mode = null;
+        String mode = "NEW";
         if (args.length < 2) {
             System.out.println("Usage: java Server <port> <players_count>");
             return;
+        } 
+        int limRounds = -1;
+        if (args.length >= 3) {
+            limRounds = Integer.parseInt(args[2]);
+            System.out.println("Limited rounds enabled ("+ limRounds +")");
         }
         int port = Integer.parseInt(args[0]);
         int playersCount = Integer.parseInt(args[1]);
+        // int port = 12345;
+        // int playersCount = 4;
 
         System.out.println("Server starting on port " + port);
         System.out.println("Waiting for " + playersCount + " players to connect...");
@@ -55,7 +66,7 @@ public class Server {
                 String playerName = "Player" + (i + 1);
                 // Create a new PlayerHandler for the connected player
                 PlayerHandler playerHandler = new PlayerHandler(playerSocket, playerName);
-                System.out.println(playerName + " connected");
+                System.out.println(playerName + " connected ( ID=" + playerHandler.hashCode() + ")");
                 // Add the player to the list
                 players.add(playerHandler);
             }
@@ -73,10 +84,6 @@ public class Server {
         }
 
         // Game logic can be added here
-        for (PlayerHandler p : players) {
-                p.send("ITEM");
-                mode = "ITEM";
-            }
         while (true) {
             // Check if player is disconnected
             players.removeIf(player -> {
@@ -101,31 +108,123 @@ public class Server {
                 }
                 return disconnected;
             });
+            if (mode == "NEW") {
+                limRounds--;
+                for (PlayerHandler p : players) {
+                    if (limRounds == -1) {
+                        System.out.println("Round limit! Kicking all players.");
+                        p.send("LIMIT");
+                        p.send("KICK");
+                        mode = "null";
+                    } else {
+                        p.send("ITEM");
+                        mode = "ITEM";
+                    }
+                }
+            }
+            if (mode == "null") {
+                System.out.println("Succesfully kicked players");
+                System.out.print("Closing server.. ");
+                break;
+            }
             // Read player responses
             if (mode == "ITEM") {
-                
+                playerItems.clear();
                 System.out.println("Waiting for players to select items...");
                 for (PlayerHandler player : players) {
                     try {
                         String response = player.receive();
                         if (response != null) {
                             System.out.println(player.playerName + " selected: " + response);
-                            // Here you can add logic to handle the player's selection
+                            playerItems.put(player, response);
                         }
                     } catch (IOException e) {
                         System.err.println("Error reading from " + player.playerName + ": " + e.getMessage());
                     }
                 }
                 System.out.println("All players have selected their items.");
-                mode = "null"; // Reset mode after processing ITEM responses
+                mode = "CH_WINNER"; // Reset mode after processing ITEM responses
                 // break;
             }
+            if (mode == "CH_WINNER") {
+                String winner = checkMatchWinner(playerItems);
+                if (winner != null) {
+                    switch (winner) {
+                        case "p" -> winsP++;
+                        case "r" -> winsK++;
+                        case "s" -> winsN++;
+                    }
+                }
+                for (PlayerHandler p : players) {
+                    if (winner != null) {
+                        p.send("WIN_"+winner);
+                    } else {
+                        p.send("DRAW");
+                    }
+                    p.send(winsP+"/"+winsK+"/"+winsN);
+                    mode = "NEW";
+                }
+            }
             try {
+                if (mode == "NEW") {
+                    System.out.println("Starting new round!");
+                    Thread.sleep(3000); // Add a 3-second delay between round
+                }
                 Thread.sleep(1000); // Add a 1-second delay between messages
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
             }
         }
+    }
+    public static String checkMatchWinner(Map<PlayerHandler, String> itemMap) {
+        int countP = 0, countK = 0, countN = 0;
+        int winP = 0, winK = 0, winN =0;
+
+        for (String value : itemMap.values()) {
+            if ("p".equals(value)) countP++;
+            else if ("r".equals(value)) countK++;
+            else if ("s".equals(value)) countN++;
+        }
+
+        // Match
+        while (countP > 0) {
+            if (countK > 0) {
+                winP++;
+                countP--;
+                countK--;
+            } else break;
+        }
+        while (countK > 0) {
+            if (countN > 0) {
+                winK++;
+                countK--;
+                countN--;
+            } else break;
+        }
+        while (countN > 0) {
+            if (countP > 0) {
+                winN++;
+                countN--;
+                countP--;
+            } else break;
+        }
+
+        String winner;
+
+        if (winP > winK && winP > winN) {
+            winner = "p";
+        } else if (winK > winP && winK > winN) {
+            winner = "r";
+        } else if (winN > winP && winN > winK) {
+            winner = "s";
+        } else {
+            winner = null;
+            System.out.println("Draw");
+        }
+
+        System.out.println("The winners are " + winner);
+
+        return winner;
     }
 }
